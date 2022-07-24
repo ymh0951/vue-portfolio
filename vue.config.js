5534b1149026820f9af3170e1529846065e0d2af
Minho Yoo <ymh0951@naver.com>
2022-07-24 18:28:41 +0900

    배포 설정

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,9 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: '/'
+  // 환경 변수들은 process.env 객체에 정의되어 있다.
+  // NODE_ENV -> 앱이 실행되는 모드 3개의 기본 모드가 있다. ("development", "production", "test")
+  publicPath: process.env.NODE_ENV === 'production'
+  ? ''
+  : '/'
 })
