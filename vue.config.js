32270421b939d369c327ce249e59429743200aca
Minho Yoo <ymh0951@naver.com>
2022-07-24 18:32:26 +0900

    배포

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,5 @@ module.exports = defineConfig({
   transpileDependencies: true,
   // 환경 변수들은 process.env 객체에 정의되어 있다.
   // NODE_ENV -> 앱이 실행되는 모드 3개의 기본 모드가 있다. ("development", "production", "test")
-  publicPath: process.env.NODE_ENV === 'production'
-  ? ''
-  : '/'
+  publicPath: '',
 })
